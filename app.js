728e05e71d4fbca664aa9824604af36ccaf7e8ff
M-wasi123 <wasikhatri11@gmail.com>
2024-03-10 04:54:52 +0500

    new

--- app.js
+++ app.js
@@ -1,31 +1,70 @@
-const tableNumber = +prompt('Table you want')
+// alert('hellow world')
 
-document.write(`<h1>Table You Want</h1>`)
+// const tableNumber = +prompt('Table you want')
 
-for (let i = 1; i < 11; i++){
-    document.write(`<li> ${tableNumber} x ${i} = ${tableNumber * i} </li>`)
-}
+// document.write(`<h1>Table You Want</h1>`)
+
+// for (let i = 1; i < 11; i++){
+//     document.write(`<li> ${tableNumber} x ${i} = ${tableNumber * i} </li>`)
+// }
 const tableNumber1 = 2
-function myfunction(){
-
+function firstclick(){
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber1} x ${i} = ${tableNumber1 * i} </li>`)
     }
 }
-const tableNumber2 = 4
-function myfunction(){
+const tableNumber2 = 3
+function secondclick(){
 
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber2} x ${i} = ${tableNumber2 * i} </li>`)
     }
 }
-const tableNumber3 = 5
-function myfunction(){
+const tableNumber3 = 4
+function thirdclick(){
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber3} x ${i} = ${tableNumber3 * i} </li>`)
     }
 }
+const tableNumber4 = 5
+function forthclick(){
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber4} x ${i} = ${tableNumber4 * i} </li>`)
+    }
+}
+const tableNumber5 = 6
+function fifthclick(){
 
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber5} x ${i} = ${tableNumber5 * i} </li>`)
+    }
+}
+const tableNumber6 = 7
+function sixthclick(){
+    for (let i = 1; i < 11; i++) 
+    {
+        document.write(`<li> ${tableNumber6} x ${i} = ${tableNumber6 * i} </li>`)
+    }
+}
+const tableNumber7 = 8
+function sevenclick(){
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber7} x ${i} = ${tableNumber7 * i} </li>`)
+    }
+}
+const tableNumber8 = 9
+function eightclick(){
+
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber8} x ${i} = ${tableNumber8 * i} </li>`)
+    }
+}
+const tableNumber9 = 10
+function nineclick(){
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber9} x ${i} = ${tableNumber9 * i} </li>`)
+    }
+}
 
 
 // document.write(`<li> ${tableNumber} x 1 = ${tableNumber * 1} </li>`)
