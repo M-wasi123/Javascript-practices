491f1b650d146fa03657447a7e82682533a7bf47
M-wasi123 <wasikhatri11@gmail.com>
2024-03-10 06:56:21 +0500

    new

--- chapters/app.js
+++ chapters/app.js
@@ -7,40 +7,57 @@
 // for (let i = 1; i < 11; i++){
 //     document.write(`<li> ${tableNumber} x ${i} = ${tableNumber * i} </li>`)
 // }
+
 const tableNumber1 = 2
 function firstclick(){
+    var back = (`<a href="./tables.html">Back</a>`)
+document.write(back)
+
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber1} x ${i} = ${tableNumber1 * i} </li>`)
     }
 }
 const tableNumber2 = 3
 function secondclick(){
-
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber2} x ${i} = ${tableNumber2 * i} </li>`)
     }
 }
 const tableNumber3 = 4
 function thirdclick(){
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber3} x ${i} = ${tableNumber3 * i} </li>`)
     }
 }
 const tableNumber4 = 5
 function forthclick(){
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber4} x ${i} = ${tableNumber4 * i} </li>`)
     }
 }
 const tableNumber5 = 6
 function fifthclick(){
-
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber5} x ${i} = ${tableNumber5 * i} </li>`)
     }
 }
 const tableNumber6 = 7
 function sixthclick(){
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++) 
     {
         document.write(`<li> ${tableNumber6} x ${i} = ${tableNumber6 * i} </li>`)
@@ -48,30 +65,41 @@ function sixthclick(){
 }
 const tableNumber7 = 8
 function sevenclick(){
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber7} x ${i} = ${tableNumber7 * i} </li>`)
     }
 }
 const tableNumber8 = 9
 function eightclick(){
-
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber8} x ${i} = ${tableNumber8 * i} </li>`)
     }
 }
 const tableNumber9 = 10
 function nineclick(){
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
+    
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${tableNumber9} x ${i} = ${tableNumber9 * i} </li>`)
     }
 }
 
 function performsearch(){
+    
     const table = document.getElementById("search-input").value;
 
     for (let i = 1; i < 11; i++){
         document.write(`<li> ${table} x ${i} = ${table * i} </li>`)
     }
+    var back = (`<a href="./tables.html">Back</a>`)
+    document.write(back)
 }
 
 // function performsearch(){
