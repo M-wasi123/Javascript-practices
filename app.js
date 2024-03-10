aca3fdba5e08edfbd402ec87838881a2ab36e30a
M-wasi123 <wasikhatri11@gmail.com>
2024-03-10 05:48:14 +0500

    new

---
+++ app.js
@@ -0,0 +1,40 @@
+const tableNumber = +prompt('Table you want')
+
+document.write(`<h1>Table You Want</h1>`)
+
+for (let i = 1; i < 11; i++){
+    document.write(`<li> ${tableNumber} x ${i} = ${tableNumber * i} </li>`)
+}
+const tableNumber1 = 2
+function myfunction(){
+
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber1} x ${i} = ${tableNumber1 * i} </li>`)
+    }
+}
+const tableNumber2 = 4
+function myfunction(){
+
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber2} x ${i} = ${tableNumber2 * i} </li>`)
+    }
+}
+const tableNumber3 = 5
+function myfunction(){
+    for (let i = 1; i < 11; i++){
+        document.write(`<li> ${tableNumber3} x ${i} = ${tableNumber3 * i} </li>`)
+    }
+}
+
+
+
+// document.write(`<li> ${tableNumber} x 1 = ${tableNumber * 1} </li>`)
+// document.write(`<li> ${tableNumber} x 2 = ${tableNumber * 2} </li>`)
+// document.write(`<li> ${tableNumber} x 3 = ${tableNumber * 3} </li>`)
+// document.write(`<li> ${tableNumber} x 4 = ${tableNumber * 4} </li>`)
+// document.write(`<li> ${tableNumber} x 5 = ${tableNumber * 5} </li>`)
+// document.write(`<li> ${tableNumber} x 6 = ${tableNumber * 6} </li>`)
+// document.write(`<li> ${tableNumber} x 7 = ${tableNumber * 7} </li>`)
+// document.write(`<li> ${tableNumber} x 8 = ${tableNumber * 8} </li>`)
+// document.write(`<li> ${tableNumber} x 9 = ${tableNumber * 9} </li>`)
+// document.write(`<li> ${tableNumber} x 10 = ${tableNumber * 10} </li>`)
