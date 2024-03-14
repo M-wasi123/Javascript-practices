d72c473b3c48e8e2bb3618633174bb7b25d165e2
M-wasi123 <wasikhatri11@gmail.com>
2024-03-14 19:46:39 +0500

    new

--- chapters/translate.js
+++ chapters/translate.js
@@ -1,18 +1,9 @@
-
-let display = document.getElementById('display');
-
-function appendToDisplay(value) {
-    display.value += value;
-}
-
-function clearDisplay() {
-    display.value = '';
-}
-
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = 'Error';
+function togglerNavbar() {
+    var navbarLinks = document.getElementById("navbarLinks");
+    if (navbarLinks.style.display === "block") {
+      navbarLinks.style.display = "none";
+    } else {
+      navbarLinks.style.display = "block";
     }
-}
+  }
+  
