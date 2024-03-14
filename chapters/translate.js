0a90e46041819b538d2d730cadcef9dabaf23491
M-wasi123 <wasikhatri11@gmail.com>
2024-03-14 20:22:28 +0500

    new

--- chapters/translate.js
+++
@@ -1,9 +0,0 @@
-function togglerNavbar() {
-    var navbarLinks = document.getElementById("navbarLinks");
-    if (navbarLinks.style.display === "block") {
-      navbarLinks.style.display = "none";
-    } else {
-      navbarLinks.style.display = "block";
-    }
-  }
-  
