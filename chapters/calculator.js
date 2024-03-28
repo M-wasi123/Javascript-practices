0aa65c60f2da75f8f0f2bcb1b8f3ddf44b2b5a9a
M-wasi123 <wasikhatri11@gmail.com>
2024-03-28 19:46:40 +0500

    new

--- chapters/calculator.js
+++ chapters/calculator.js
@@ -1,15 +1,16 @@
 function display(val) {
     document.getElementById('result').value += val;
 }
-function solve() {
+function display1() {
+    document.getElementById('result').value ;
+}
+function solve1() {
     let x = document.getElementById('result').value;
     let y = eval(x);
     document.getElementById('result1').value =y;
-
-    if (y) {
-        document.getElementById('result').value ='';  
-    }
+        document.getElementById('result').value = ''; 
 }
+
 function clearScreen1() {
     document.getElementById('result1').value =''
 }
